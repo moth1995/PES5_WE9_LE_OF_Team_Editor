60bf3aa029df08dab21ac212ce87a4972c1464f8
moth1995 <36039932+moth1995@users.noreply.github.com>
2021-02-15 10:13:28 -0300

    many changes in here read description
    Added extra tab with encrypt decrypt buttons
    no more temporary file creation needed

--- main.py
+++ main.py
@@ -15,6 +15,23 @@ from swap_teams import encrypt_and_save
 from player_data import get_stats, set_value, get_value
 from export_csv import write_csv
 from import_csv import load_csv
+from of_crypt import of_encrypter, of_decrypter
+
+def decrypt_btn_action():
+    root.temp_file = filedialog.asksaveasfile(initialdir=".",title="Create your decrypted OF", mode='wb', filetypes=(("Bin files","*.bin"),("All files", "*")), defaultextension=".bin")
+    if of_decrypter(of, root.temp_file.name):
+        messagebox.showinfo(title=appname,message="Decrypted OF created!")
+    else:
+        messagebox.showerror(title=appname,message="Error while creating file, please run as admin")
+
+def encrypt_btn_action():
+    messagebox.showinfo(title=appname,message="Please take in mind that this will overwrite the data from the OF selected at start with the data from decrypted OF!")
+    root.temp_file = filedialog.askopenfilename(initialdir=".",title="Select your decrypted OF", filetypes=(("Bin files","*.bin"),("All files", "*")))
+    if root.temp_file!="":
+        if of_encrypter(root.temp_file, of):
+            messagebox.showinfo(title=appname,message="OF encrypted!")
+        else:
+            messagebox.showerror(title=appname,message="Error while reading file, please run as admin")
 
 
 def export_all_to_csv():
@@ -46,7 +63,7 @@ def swap_list_positions(teams_list, pos1, pos2):
     return teams_list
 
 def swap_btn_action():
-    if swap_teams_data(temp_file,team_a_cmb.current(),team_b_cmb.current()):
+    if swap_teams_data(of.data,team_a_cmb.current(),team_b_cmb.current()):
         global teams_list
         teams_list=swap_list_positions(teams_list, team_a_cmb.current(), team_b_cmb.current())
         team_a_cmb.config(values=teams_list)
@@ -54,9 +71,8 @@ def swap_btn_action():
         messagebox.showinfo(title=appname,message="Teams swapped!")        
     else:
         messagebox.showerror(title=appname,message="Can't swap the same team!!!")
-
 def save_btn_action():
-    if encrypt_and_save(temp_file,of):
+    if encrypt_and_save(of):
         messagebox.showinfo(title=appname,message="All changes saved")
     else:
         messagebox.showerror(title=appname,message="Error while saving, please run as admin")
@@ -77,16 +93,19 @@ y = (hs/2) - (h/2)
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 #Once it start it will ask to select the option file
 root.filename=""
-temp_file=""
+#temp_file=""
 root.filename = filedialog.askopenfilename(initialdir=".",title="Select your option file", filetypes=(("KONAMI-WIN32PES5OPT","KONAMI-WIN32PES5OPT"),("KONAMI-WIN32WE9UOPT","KONAMI-WIN32WE9UOPT"),("KONAMI-WIN32WE9KOPT","KONAMI-WIN32WE9KOPT")))
 #root.filename = "KONAMI-WIN32PES5OPT"
 #root.filename = r"C:\Users\marco\Documents\KONAMI\Pro Evolution Soccer 5\save\folder1\KONAMI-WIN32PES5OPT"
 if root.filename!="":
     of = OptionFile(root.filename)
+    '''
+    # No need anymore to create a temporary file, we just use the bytearray
     temp_file = "temp.bin"
     with open(temp_file, "wb") as binary_file:
         binary_file.write(of.data)
         #print("Temporary file created")
+    '''
 else:
     root.destroy()
 
@@ -112,7 +131,8 @@ print(all_data)
 
 tabs_container=ttk.Notebook(root)
 swap_teams_tab=Frame(tabs_container, width=w, height=h)
-export_team_tab=Frame(tabs_container, width=w, height=h)
+csv_tab=Frame(tabs_container, width=w, height=h)
+extra_tab=Frame(tabs_container, width=w, height=h)
 
 #Swap teams tab 
 
@@ -130,11 +150,16 @@ save_changes_btn=Button(swap_teams_tab, text="Save changes", command=lambda: sav
 copyright_lbl=Label(swap_teams_tab, text="By PES Indie Team")
 thanks_lbl=Label(swap_teams_tab, text="Thanks to PeterC10 for python de/encrypt code for OF")
 
-#Export team tab
+#CSV tab
 
-#wip_lbl=Label(export_team_tab, text="Still working on this section, soon there will be something to test")
-create_csv_btn=Button(export_team_tab, text="Create CSV", command=lambda: export_all_to_csv())
-import_csv_btn=Button(export_team_tab, text="Import CSV", command=lambda: import_all_from_csv())
+#wip_lbl=Label(csv_tab, text="Still working on this section, soon there will be something to test")
+create_csv_btn=Button(csv_tab, text="Create CSV", command=lambda: export_all_to_csv())
+import_csv_btn=Button(csv_tab, text="Import CSV", command=lambda: import_all_from_csv())
+
+#extra tab
+
+decrypt_of_btn=Button(extra_tab, text="Decrypt", command=lambda: decrypt_btn_action())
+encrypt_of_btn=Button(extra_tab, text="Encrypt", command=lambda: encrypt_btn_action())
 
 
 #Swap team tab placing
@@ -148,18 +173,31 @@ save_changes_btn.place(x=376, y=200)
 copyright_lbl.place(x=0, y=555)
 thanks_lbl.place(x=500, y=555)
 
-#Export team tab placing
+#CSV tab placing
 
 #wip_lbl.place(x=280, y=160)
 create_csv_btn.place(x=240, y=160)
 import_csv_btn.place(x=320, y=160)
+
+# Extra tab placing
+
+decrypt_of_btn.place(x=240, y=160)
+encrypt_of_btn.place(x=320, y=160)
+
+
 #Placing tabs and container in the root
+
+
+
 
 tabs_container.pack()
 swap_teams_tab.pack(fill="both", expand=1)
-export_team_tab.pack(fill="both", expand=1)
+csv_tab.pack(fill="both", expand=1)
+extra_tab.pack(fill="both", expand=1)
+
 tabs_container.add(swap_teams_tab, text="Swap Teams")
-tabs_container.add(export_team_tab, text="Export/Import Team")
+tabs_container.add(csv_tab, text="Export/Import CSV")
+tabs_container.add(extra_tab, text="Extra")
 
 root.resizable(False, False)
 root.mainloop() 
